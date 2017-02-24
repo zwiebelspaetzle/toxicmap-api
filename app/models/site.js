43db879d5dcec8e6d31fc585ccae139cc0a752f9
Chris Ward <cward@Chris-Ward-MBP.local>
2017-02-24 08:23:15 -0700

    api with find by site id

---
+++ app/models/site.js
@@ -0,0 +1,25 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var SiteSchema = new Schema({
+  _id            : Schema.Types.ObjectId,
+  region         : String,
+  siteId         : String,
+  epaId          : String,
+  name           : String,
+  streetAddress  : String,
+  streetAddress2 : String,
+  city           : String,
+  state          : String,
+  zip            : String,
+  congDistrict   : String,
+  county         : String,
+  fips           : String,
+  lat            : String,
+  lon            : String,
+  npl            : String,
+  ff             : String,
+  nonNplStatus   : String
+});
+
+module.exports = mongoose.model('active-sites', SiteSchema);
